8644a561c52595fb23ea0cbf02f630bf839e966d
EleanorMao <danningmao@outlook.com>
2016-08-24 10:23:31 +0800

    Update ellipsis.js

--- ellipsis.js
+++ ellipsis.js
@@ -2,7 +2,7 @@ function ellipsis(selector, innerSelector){
     var doms = Array.prototype.slice.call(document.querySelectorAll(selector), 0);
     doms.map(function(item){
       let outerHeight = item.offsetHeight;
-      let innerText = item.querySelector('innerSelector');
+      let innerText = item.querySelector(innerSelector);
       if(!innerText) return;
       while(innerText.offsetHeight > outerHeight){
         let text = innerText.innerText;
